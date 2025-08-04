770fcfbf4e20e611a712046a12ba79fde2973858
Kevin Naphan <kevinnaphan@Kevins-MacBook-Pro.local>
2025-08-04 22:35:04 +0700

    small change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["wp.still-bloom.my.id"],
+  },
 };
 
 export default nextConfig;
+
+
